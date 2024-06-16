b6b2906b46331d4060399d35a0ad7983ad9e806e
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-06-16 23:46:14 +0200

    done with modal

--- script.js
+++ script.js
@@ -132,28 +132,3 @@ function submitTwo() {
   greeting();
   sliderValue();
 }
-
-// --------- COURSES ----------//
-
-var get = $.ajax({
-  type: 'GET',
-  url: 'http://localhost:3000/courses',
-});
-
-get.done(function (podaci) {
-  $.each(podaci, function (i, podatak) {
-    $('#tbody').append(
-      `<tr><td>${podatak.course}</td>
-      <td>${podatak.starting_date}</td>
-      <td>${podatak.duration}</td>
-      <td>${podatak.price}</td>
-      <td><button id="'podatak.id + '" class="btn btn-outline-info btnDetailsColor">View Details</button></td>
-      <td><button id="'podatak.id + '" class="btn btn-outline-success btnDetailsColor">Make Reservation</button></td></tr>
-      `
-    );
-  });
-  $('#table').dataTable();
-});
-get.fail(function (podaci) {
-  alert(podaci.statusText);
-});
