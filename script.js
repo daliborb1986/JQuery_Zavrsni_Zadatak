214336d0010ea87066bc591ce827e8dbb36cb430
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-06-16 20:59:22 +0200

    done with html and table

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ btnPage3.addEventListener('click', goToPage4);
 btnPage4.addEventListener('click', goToPage5);
 btnSubmit.addEventListener('click', submitTwo);
 
-
 // ---- Functions ----- //
 
 function checkFirstName() {
@@ -133,3 +132,28 @@ function submitTwo() {
   greeting();
   sliderValue();
 }
+
+// --------- COURSES ----------//
+
+var get = $.ajax({
+  type: 'GET',
+  url: 'http://localhost:3000/courses',
+});
+
+get.done(function (podaci) {
+  $.each(podaci, function (i, podatak) {
+    $('#tbody').append(
+      `<tr><td>${podatak.course}</td>
+      <td>${podatak.starting_date}</td>
+      <td>${podatak.duration}</td>
+      <td>${podatak.price}</td>
+      <td><button id="'podatak.id + '" class="btn btn-outline-info btnDetailsColor">View Details</button></td>
+      <td><button id="'podatak.id + '" class="btn btn-outline-success btnDetailsColor">Make Reservation</button></td></tr>
+      `
+    );
+  });
+  $('#table').dataTable();
+});
+get.fail(function (podaci) {
+  alert(podaci.statusText);
+});
