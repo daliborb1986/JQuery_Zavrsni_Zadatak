b1a0a025e88e1aa32e22a21cb4b67d1de89e8c1e
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-06-19 23:13:01 +0200

    done with form validation

--- jquery.js
+++ jquery.js
@@ -11,7 +11,7 @@ get.done(function (podaci) {
         <td>${podatak.duration}</td>
         <td>${podatak.price}</td>
         <td><button id="btn${podatak.id}Info" class="btn btn-outline-info btnDetailsColor" onclick="infoCourse(${podatak.id}, event)">View Details</button></td>
-        <td><button id="btn${podatak.id}" class="btn btn-outline-success btnDetailsColor">Make Reservation</button></td></tr>
+        <td><button id="btn${podatak.id}Reservation" class="btn btn-outline-success btnDetailsColor"onclick="reserveCourse(${podatak.id})">Make Reservation</button></td></tr>
         `
     );
   });
@@ -28,7 +28,66 @@ function infoCourse(courseId) {
     success: function (course) {
       $('#modalDetails').modal('toggle');
       $('#modalLabel').text(course.course);
-      $('#modalPrice').text(`${course.price} $`);
+      $('#modalPrice').text(`${course.price}$`);
+    },
+  });
+}
+
+function reserveCourse(id) {
+  var getRequest = $.ajax({
+    type: 'GET',
+    url: `http://localhost:3000/courses/${id}`,
+  });
+
+  getRequest.done(function (course) {
+    $('#selectedCourse').val(course.course);
+    $('#price').val(course.price + '$');
+    $('#modalUnosForma').modal('toggle');
+    setValidationForm();
+  });
+
+  getRequest.fail(function (error) {
+    alert('Failed to fetch course details');
+  });
+}
+function setValidationForm() {
+  var selectedCourse = $('#selectedCourse').val();
+  var price = $('#price').val();
+  $('#modalForm').trigger('reset');
+  $('#selectedCourse').val(selectedCourse);
+  $('#price').val(price);
+  $('#modalForm').validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 3,
+      },
+      surname: {
+        required: true,
+        minlength: 3,
+      },
+      email: {
+        required: true,
+      },
+      note: {
+        required: true,
+        minlength: 5,
+      },
+    },
+    messages: {
+      name: {
+        required: 'Unesite vase ime',
+        minlength: 'Ime mora imati najmanje 3 karaktera',
+      },
+      surname: {
+        required: 'Unesite svoje prezime',
+        minlength: 'Prezime mora imati najmanje 3 karaktera',
+      },
+      email: 'Unesite validan e-mail',
+      note: {
+        required: 'Unesite komentar',
+        minlength: 'Komentar mora imati najmanje 5 karaktera',
+      },
     },
   });
 }
